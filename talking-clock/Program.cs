7ba0f882fe507a9e6b26a79545fb20172e2b7d76
Makolicious <m.aleksandarr@gmail.com>
2017-10-11 20:11:36 +1100

    Add project files.

---
+++ talking-clock/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talking_clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
